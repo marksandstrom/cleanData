c09f2a4fb5531665f8b229882ec74621de6d5be1
marksandstrom <marksandstrom@users.noreply.github.com>
2015-05-23 15:49:00 +0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,2 +1,39 @@
+trainTable = read.table("X_train.txt")
+testTable = read.table("X_test.txt")
+table = rbind(trainTable,testTable)
 
-url <- 
+featTable = read.table("features.txt")
+features = t(featTable[2])
+names(table) = features
+meanTable = table[,c(colnames(table)[grep("mean",colnames(table))])]
+stdTable = table[,c(colnames(table)[grep("std",colnames(table))])]
+meanStdTable = cbind(meanTable,stdTable)
+mst = na.omit(meanStdTable)
+
+actrain = read.table("y_train.txt")
+actest = read.table("y_test.txt")
+acs = rbind(actrain,actest)
+subtrain = read.table("subject_train.txt")
+subtest = read.table("subject_test.txt")
+subs = rbind(subtrain,subtest)
+
+tbl = cbind(acs,subs,mst)
+lbls = read.table("activity_labels.txt")
+albls = lbls[,2]
+acode = lbls[,1]
+maxa = max(acode)
+maxs = max(subs)
+res = data.frame()
+for (a in 1:maxa) {
+  ACT = lbls[a]
+  for (s in 1:maxs) {
+    st = tbl[tbl[,1]==a & tbl[,2]==s,] # sub-table matching activity a and subject s
+    t = st[,-(1:2)] 
+    means = t(colMeans(t))
+    row = cbind(ACT,s,means)
+    res = rbind(res,row)
+  }
+}
+names(res)[1]='ACTIVITY'
+names(res)[2]='SUBJECT ID'
+res
